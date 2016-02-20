5d044b1f07fa0d14433af2cc4a1d609fef6ccce3
dm.kuznetsov <dn.kuznetsov@rambler-co.ru>
2016-02-20 19:03:34 +0300

    update modules

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,6 @@
 import React, {AppRegistry, Component} from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/store';
-import Raven from 'raven-js';
 import Router from './src/router';
 
 /*Raven.config('http://6f138f13e4754b139769570b528c0220@sentry01.family.rambler.ru/8', {
